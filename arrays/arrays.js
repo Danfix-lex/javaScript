480d7d90ce189f212cb7d6617fae787c4725129f
Danfix-lex <danielojolex@gmail.com>
2025-02-04 15:19:27 +0100

    java Script

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1,8 +1,13 @@
 const array = []; // LITERAL METHOD
 
 const arrayOfNumbers = new Array(2); // ARRAY CONSTRUCTOR
+arrayOfNumbers[0] = 20;
+arrayOfNumbers[1] = 2;
 arrayOfNumbers.push(1);
 arrayOfNumbers.push(2);
 arrayOfNumbers.push(3);
 
-console.log(arrayOfNumbers);
+let firstName = "Daniel";
+
+let newArray = Array.from(firstName);
+console.log(newArray);
