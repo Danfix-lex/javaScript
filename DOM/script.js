ad8fff22b9f202b8cff9913dff10df2178d149f2
Danfix-lex <danielojolex@gmail.com>
2025-02-25 14:19:25 +0100

    java script

--- DOM/script.js
+++ DOM/script.js
@@ -13,3 +13,31 @@ titles.forEach((title)=>{
     title.textContent = "Danfix's Book Collection";
     console.log(title.textContent);
 })
+
+const name = document.getElementsByClassName('name')
+const names = Array.from(name)
+names.forEach((name)=>{
+    names[0].textContent = "Jagun Jagun";
+    console.log(names[1]);
+})
+
+const nameOne = document.getElementsByClassName('name')
+const namesOne = Array.from(nameOne)
+namesOne.forEach((name)=>{
+    namesOne[1].textContent = "Oloriogun";
+    console.log(namesOne[1]);
+})
+
+const nameTwo = document.getElementsByClassName('name')
+const namesTwo = Array.from(nameTwo)
+namesTwo.forEach((name)=>{
+    namesTwo[2].textContent = "Koto Aye";
+    console.log(namesTwo[2]);
+})
+
+const nameThree = document.getElementsByClassName('name')
+const namesThree = Array.from(nameThree)
+namesThree.forEach((name)=>{
+    namesThree[3].textContent = "Iberu Obi";
+    console.log(namesThree[3]);
+})
